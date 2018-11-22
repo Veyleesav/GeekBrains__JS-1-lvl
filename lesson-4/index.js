300b1e68963acc08910f92c6d093022d39a81649
Lev Vasilyev <wanbergg@gmail.com>
2018-11-22 22:03:50 +0300

    lesson-4
    main logic

---
+++ lesson-4/index.js
@@ -0,0 +1,17 @@
+const foo = (...elements) => {
+    let sum=0;
+    if(elements.length>0){
+        if(elements.length===1){
+            sum=elements[0];
+        }
+        else{
+            sum=1;
+            for(let i=0;i<elements.length;i++){
+                sum*=elements[i];
+            }
+        }
+
+    }
+    return sum;
+};
+console.log(foo(2,2));
