48311f58792cc7b156df76bec38345f6ab151112
Lev Vasilyev <wanbergg@gmail.com>
2018-11-26 11:49:24 +0300

    final v1.0
    done

--- lesson-4/index.js
+++ lesson-4/index.js
@@ -1,18 +1,21 @@
-
-const foo = (...elements) => {
-    let sum=0;
-    if(arguments.length>0){
-        if(elements.length===1){
-            sum=elements[0];
+let sum = 1;
+let total = 0;
+function foo(arg) {
+    if(Number.isInteger(arg)){
+    sum *= arg;
+    return function fooMulti(num){
+        if(num !== undefined){
+        return total=foo(num);
         }
-        else{
-            sum=1;
-            for(let i=0;i<elements.length;i++){
-                sum*=elements[i];
-            }
-        }
-
+        let final = sum;
+        sum = 1;  //Обнуляем переменные в конце работы функции для того, чтобы можно было её многократно использовать.
+        total = 0;
+        return final;
     }
-    return sum;
-};
-console.log(foo(2, 4));
+    }
+}
+
+
+console.log(foo(5)(2)());
+console.log(foo(5)());
+console.log(foo(2)(1)(3)(4)());
