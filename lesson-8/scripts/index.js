26eb1c24524558da9bee76833e34dcbed79dad3d
Lev Vasilyev <wanbergg@gmail.com>
2018-12-03 19:23:42 +0300

    lesson 8.2
    task

---
+++ lesson-8/scripts/index.js
@@ -0,0 +1,5 @@
+// const firstEl=document.getElementById("iden");
+//const firstEl=document.getElementsByClassName("class")[0];
+const firstEl = document.querySelector("div");
+firstEl.innerText= "I am here!";
+console.log(firstEl);
