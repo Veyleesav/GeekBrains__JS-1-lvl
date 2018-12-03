70347fcf46dc7ddab0cf4925518934a124acd2f2
Lev Vasilyev <wanbergg@gmail.com>
2018-12-03 20:12:36 +0300

    lesson 8.3

--- lesson-8/scripts/index.js
+++ lesson-8/scripts/index.js
@@ -1,5 +1,7 @@
 // const firstEl=document.getElementById("iden");
 //const firstEl=document.getElementsByClassName("class")[0];
 const firstEl = document.querySelector("div");
-firstEl.innerText= "I am here!";
+// firstEl.innerText= "I am here!";
+firstEl.innerHTML="I`m <strong>here</strong>!";
+firstEl.classList.add("added");
 console.log(firstEl);
