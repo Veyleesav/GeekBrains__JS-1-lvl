330ef5bb318b6982172190e20b38bfccd55e9c70
Lev Vasilyev <wanbergg@gmail.com>
2018-12-07 13:37:54 +0300

    lesson 9.5

--- lesson-9/scripts/Event-listener.js
+++ lesson-9/scripts/Event-listener.js
@@ -9,6 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".categories").appendChild(newEl);
     }
 });
+
+function getFilmComments(filmName){
+    const film = films.filter( f => f.name === filmName )[0];
+    return film.comments;
+}
+
 function onCategoryChoice(categoryName) {
     document.querySelector(".films").innerHTML = "";
     const films = getFilmsByCategory(categoryName);
@@ -18,7 +24,12 @@ function onCategoryChoice(categoryName) {
         newEl.innerHTML = `<div class="film-name">${film.name}</div>`;
 
         newEl.addEventListener("click", function () {
-            newEl.innerHTML +=`<div class="film-comments">film clicked</div>`;
+            const comments = getFilmComments(film.name);
+            let s = "";
+            comments.forEach(c => {
+                s+=`<div class="comment">${c.text}</div>`
+            });
+            newEl.innerHTML +=`<div class="film-comments">${s}</div>`;
         });
 
         document.querySelector(".films").appendChild(newEl);
